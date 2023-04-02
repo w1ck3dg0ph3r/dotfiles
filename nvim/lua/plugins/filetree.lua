278d2fd7042b609756739d39987e8851d54bf80b
w1ck3dg0ph3r <w1ck3dg0ph3r@gmail.com>
2023-04-02 11:19:21 +0400

    neovim: more nvim-tree config

--- nvim/lua/plugins/filetree.lua
+++ nvim/lua/plugins/filetree.lua
@@ -1,11 +1,25 @@
 return {
   'nvim-tree/nvim-tree.lua',
-  dependencies = {'nvim-tree/nvim-web-devicons'},
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     require('nvim-tree').setup({
       sort_by = 'case_sensitive',
+      view = {
+        mappings = {
+          custom_only = false,
+          list = {
+            { key = '<space>', action = 'edit' },
+            { key = 'E', action = 'expand_all' },
+            { key = 'H', action = 'collapse_all' },
+          },
+        },
+      },
+      diagnostics = { enable = true },
     })
 
+    local api = require('nvim-tree.api')
+
+    -- Open tree on start
     local function open_nvim_tree(data)
       -- buffer is a directory
       local directory = vim.fn.isdirectory(data.file) == 1
@@ -15,30 +29,20 @@ return {
       -- change to the directory
       vim.cmd.cd(data.file)
       -- open the tree
-      require('nvim-tree.api').tree.open()
+      api.tree.open()
     end
-
     vim.api.nvim_create_autocmd({ 'VimEnter' }, { callback = open_nvim_tree })
+
+    -- Follow current buffer
+    local function auto_update_path()
+      local buf = vim.api.nvim_get_current_buf()
+      local bufname = vim.api.nvim_buf_get_name(buf)
+      if vim.fn.isdirectory(bufname) or vim.fn.isfile(bufname) then
+        require('nvim-tree.api').tree.find_file(vim.fn.expand('%:p'))
+      end
+    end
+    vim.api.nvim_create_autocmd('BufEnter', { callback = auto_update_path })
 
     vim.keymap.set('n', '<leader>t', '<cmd>NvimTreeToggle<cr>')
   end,
 }
-
--- return {
---   'nvim-neo-tree/neo-tree.nvim',
---   dependencies = {'nvim-lua/plenary.nvim', 'MunifTanjim/nui.nvim', 'nvim-tree/nvim-web-devicons'},
---   keys = {
---     {'<leader>t', '<cmd>Neotree toggle<cr>', desc='NeoTree'},
---   },
---   config = function()
---     require('neo-tree').setup({
---       filesystem = {
---         follow_current_file = true,
---       },
---       source_selector = {
---         winbar = false,
---         statusline = false,
---       }
---     })
---   end,
--- }
