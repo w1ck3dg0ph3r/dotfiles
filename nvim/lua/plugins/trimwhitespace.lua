731a1c834e334702c6d13b8f8e98f13ccdb0f5c7
w1ck3dg0ph3r <w1ck3dg0ph3r@gmail.com>
2023-05-24 11:38:25 +0400

    neovim: trim whitespace on write

---
+++ nvim/lua/plugins/trimwhitespace.lua
@@ -0,0 +1,10 @@
+return {
+  'cappyzawa/trim.nvim',
+  opts = {
+    ft_blocklist = { 'markdown' },
+    trim_on_write = true,
+    trim_trailing = true,
+    trim_last_line = true,
+    trim_first_line = true,
+  },
+}
