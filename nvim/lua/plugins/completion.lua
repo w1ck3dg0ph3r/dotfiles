376d4ce3f97a3e5203cfbfd898bafb46cfadca93
w1ck3dg0ph3r <w1ck3dg0ph3r@gmail.com>
2023-05-28 09:08:23 +0400

    neovim: tweak completion behaviour

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -20,11 +20,6 @@ return {
       local cmp = require('cmp')
       local luasnip = require('luasnip')
 
-      local has_words_before = function()
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
-      end
-
       local kind_icons = {
         Text = '',
         Method = 'm',
@@ -74,8 +69,6 @@ return {
               cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then
               luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
             else
               fallback()
             end
@@ -110,7 +103,9 @@ return {
           { name = 'luasnip' },
           { name = 'buffer' },
           { name = 'path' },
-        })
+        }),
+
+        preselect = cmp.PreselectMode.None,
       })
 
       require('luasnip.loaders.from_vscode').lazy_load()
