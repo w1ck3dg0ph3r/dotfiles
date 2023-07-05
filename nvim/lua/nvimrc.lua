26c4e6f5e92c483cb84334d07196e7bff006d4b5
w1ck3dg0ph3r <w1ck3dg0ph3r@gmail.com>
2023-07-05 09:23:51 +0400

    neovim: better error handling in nvimrc

--- nvim/lua/nvimrc.lua
+++ nvim/lua/nvimrc.lua
@@ -8,12 +8,12 @@ M.config = function()
   end
 
   local nvimrc_path = vim.fn.findfile('.nvimrc.lua', '.;')
-  if nvimrc_path ~= nil then
+  if nvimrc_path ~= nil and nvimrc_path ~= '' then
     local ok, config = pcall(dofile, nvimrc_path)
     if ok then
       nvimrc_config = config
     else
-      print(config)
+      print('error loading "' .. nvimrc_path .. '": ' .. config)
       nvimrc_config = {}
     end
   end
