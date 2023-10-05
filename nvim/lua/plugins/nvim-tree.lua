f0d6248ed7d08a7b3374c72b921e772ceba4319c
w1ck3dg0ph3r <w1ck3dg0ph3r@gmail.com>
2023-10-05 09:31:21 +0400

    neovim: use update_focused_file nvim-tree option

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -13,6 +13,9 @@ local plugin = {
     view = {
       width = 40,
     },
+    update_focused_file = {
+      enable = true,
+    },
     renderer = {
       root_folder_label = ':t',
       icons = {
@@ -49,16 +52,6 @@ local plugin = {
     vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<cr>')
 
     local api = require('nvim-tree.api')
-
-    -- Follow current buffer
-    local function auto_update_path()
-      local buf = vim.api.nvim_get_current_buf()
-      local bufname = vim.api.nvim_buf_get_name(buf)
-      if vim.fn.isdirectory(bufname) or vim.fn.isfile(bufname) then
-        require('nvim-tree.api').tree.find_file(vim.fn.expand('%:p'))
-      end
-    end
-    vim.api.nvim_create_autocmd('BufEnter', { callback = auto_update_path })
 
     -- Go to last used hidden buffer when deleting a buffer
     vim.api.nvim_create_autocmd('BufEnter', {
