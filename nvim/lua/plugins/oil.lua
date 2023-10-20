d4e0290543d7e736ef9b96dc954ac037f0288401
w1ck3dg0ph3r <w1ck3dg0ph3r@gmail.com>
2023-10-20 11:52:56 +0400

    neovim: add oil

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,34 @@
+return {
+  'stevearc/oil.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+
+  config = function()
+    local oil = require('oil')
+    local util = require('util')
+
+    oil.setup({
+      columns = {
+        -- 'type',
+        'icon',
+        -- 'permissions',
+        -- 'size',
+        -- 'mtime',
+      },
+      -- buf_options = {
+      --   buflisted = true,
+      --   bufhidden = '',
+      -- },
+      use_default_keymaps = false,
+      keymaps = {
+        ['g?'] = 'actions.show_help',
+        ['<CR>'] = 'actions.select',
+        ['<C-p>'] = 'actions.preview',
+        ['q'] = 'actions.close',
+        ['gs'] = 'actions.change_sort',
+        ['g.'] = 'actions.toggle_hidden',
+      },
+    })
+
+    util.map('n', '-', '<cmd>Oil<cr>')
+  end,
+}
