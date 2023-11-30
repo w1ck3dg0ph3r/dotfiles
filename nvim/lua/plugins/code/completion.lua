a951c9b250d633a28011fbd62b6855bdb399bace
w1ck3dg0ph3r <w1ck3dg0ph3r@gmail.com>
2023-11-30 09:34:15 +0400

    neovim: tinker some more with completion sorting

--- nvim/lua/plugins/code/completion.lua
+++ nvim/lua/plugins/code/completion.lua
@@ -98,21 +98,26 @@ return {
       },
 
       sources = cmp.config.sources({
-        { name = 'nvim_lsp' },
-        { name = 'luasnip' },
-        { name = 'path' },
-        { name = 'buffer' },
+        { priority = 10, name = 'nvim_lsp' },
+        { priority = 20, name = 'luasnip' },
+        { priority = 05, name = 'path' },
+        { priority = 01, name = 'buffer' },
       }),
 
       ---@diagnostic disable-next-line: missing-fields
       sorting = {
+        priority_weight = 1.0,
         comparators = {
-          cmp.config.compare.score,
-          cmp.config.compare.exact,
-          cmp.config.compare.kind,
-          cmp.config.compare.length,
-          cmp.config.compare.order,
-          cmp.config.compare.offset,
+          cmp.config.compare.score,         -- Entries with higher score will be ranked higher.
+          -- cmp.config.compare.locality,      -- Entries with higher locality (i.e., words that are closer to the cursor) will be ranked higher. See GH-183 for more details.
+          -- cmp.config.compare.recently_used, -- Entries that are used recently will be ranked higher.
+          cmp.config.compare.exact,         -- Entries with exact == true will be ranked higher.
+          cmp.config.compare.length,        -- Entires with shorter label length will be ranked higher.
+          -- cmp.config.compare.kind,          -- Entires with smaller ordinal value of 'kind' will be ranked higher.
+          -- cmp.config.compare.order,         -- Entries with smaller id will be ranked higher.
+          -- cmp.config.compare.offset,        -- Entries with smaller offset will be ranked higher.
+          -- cmp.config.compare.sort_text,     -- Entries will be ranked according to the lexicographical order of sortText.
+          -- cmp.config.compare.scopes,        -- Entries defined in a closer scope will be ranked higher (e.g., prefer local variables to globals).
         }
       },
 
