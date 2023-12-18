353b9e66de9d31093939ad079c33f8c5d03c2567
w1ck3dg0ph3r <w1ck3dg0ph3r@gmail.com>
2023-12-18 09:42:25 +0400

    neovim: limit treesitter context height

--- nvim/lua/plugins/code/context.lua
+++ nvim/lua/plugins/code/context.lua
@@ -4,6 +4,8 @@ return {
   event = { 'BufNewFile', 'BufReadPost', 'FileReadPost' },
 
   opts = {
+    max_lines = 5,
+    multiline_threshold = 1,
     on_attach = function(bufnr)
       if vim.b[bufnr].big_file then return false end
       return true
