a396c3d99c1838a9c139fd1b09f9f5322efc77f0
w1ck3dg0ph3r <w1ck3dg0ph3r@gmail.com>
2024-07-16 11:24:39 +0400

    neovim: run volar lsp server in hybrid mode with tsserver

--- nvim/lua/plugins/code/lsp.lua
+++ nvim/lua/plugins/code/lsp.lua
@@ -1,73 +1,3 @@
-local servers = {
-  lua_ls = {},
-  bashls = {},
-  gopls = {
-    settings = {
-      gopls = {
-        gofumpt = true,
-        semanticTokens = true,
-        experimentalPostfixCompletions = true,
-        usePlaceholders = false,
-        hints = {
-          compositeLiteralFields = true,
-          parameterNames = true,
-        },
-        directoryFilters = { '-vendor' },
-      },
-    },
-  },
-  volar = {
-    filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
-    settings = {},
-  },
-}
-
-local formatters = {
-  go = 'gopls',
-  lua = 'lua_ls',
-  cpp = 'clangd',
-  c = 'clangd',
-  rust = 'rust_analyzer',
-  python = 'null-ls',
-}
-for _, filetype in ipairs({ 'javascript', 'typescript', 'vue', 'html', 'pug', 'css', 'scss', 'sass', 'json', 'yaml' }) do
-  formatters[filetype] = 'null-ls'
-end
-
----@diagnostic disable-next-line: unused-local
-local on_attach = function(client, bufnr)
-  local util = require('util')
-
-  local bufopts = { noremap = true, silent = true, buffer = bufnr }
-  local has_trouble, _ = pcall(require, 'trouble')
-
-  util.map('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  util.map('n', 'gd', vim.lsp.buf.definition, bufopts)
-  -- util.map('n', 'K', vim.lsp.buf.hover, bufopts) -- Defined in keymap.lua
-  if not has_trouble then
-    util.map('n', 'gr', vim.lsp.buf.references, bufopts)
-    util.map('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  end
-  util.map('i', '<c-k>', vim.lsp.buf.signature_help, bufopts)
-  util.map('n', '<leader>T', vim.lsp.buf.type_definition, bufopts)
-  util.map('n', '<leader>R', vim.lsp.buf.rename, bufopts)
-  util.map('n', '<leader>ac', vim.lsp.buf.code_action, bufopts)
-
-  local filetype = vim.bo[bufnr].filetype
-  if formatters[filetype] ~= nil then
-    util.map(
-      'n', '<leader>f',
-      function()
-        vim.lsp.buf.format({ name = formatters[filetype] })
-        vim.api.nvim_feedkeys('zx', 'n', false)
-      end,
-      bufopts
-    )
-  else
-    util.map('n', '<leader>f', function() print('no formatter configured for ' .. filetype) end, bufopts)
-  end
-end
-
 return {
   'neovim/nvim-lspconfig',
 
@@ -84,6 +14,97 @@ return {
     local mason = require('mason')
     local masonconfig = require('mason-lspconfig')
     mason.setup()
+
+    local servers = {
+      lua_ls = {},
+      bashls = {},
+      gopls = {
+        settings = {
+          gopls = {
+            gofumpt = true,
+            semanticTokens = true,
+            experimentalPostfixCompletions = true,
+            usePlaceholders = false,
+            hints = {
+              compositeLiteralFields = true,
+              parameterNames = true,
+            },
+            directoryFilters = { '-vendor' },
+          },
+        },
+      },
+      tsserver = {
+        filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
+        init_options = {
+          plugins = {
+            {
+              name = '@vue/typescript-plugin',
+              location = require('mason-registry').get_package('vue-language-server'):get_install_path()
+                  .. '/node_modules/@vue/language-server',
+              languages = { 'vue' },
+            }
+          },
+        },
+      },
+      volar = {
+        settings = {
+          vue = {
+            complete = {
+              casing = {
+                tags = 'pascal',
+                props = 'camel',
+              },
+            },
+          },
+        },
+      },
+    }
+
+    local formatters = {
+      go = 'gopls',
+      lua = 'lua_ls',
+      cpp = 'clangd',
+      c = 'clangd',
+      rust = 'rust_analyzer',
+      python = 'null-ls',
+    }
+    for _, filetype in ipairs({ 'typescript', 'javascript', 'vue', 'html', 'pug', 'css', 'scss', 'sass', 'json', 'yaml' }) do
+      formatters[filetype] = 'null-ls'
+    end
+
+    ---@diagnostic disable-next-line: unused-local
+    local on_attach = function(client, bufnr)
+      local util = require('util')
+
+      local bufopts = { noremap = true, silent = true, buffer = bufnr }
+      local has_trouble, _ = pcall(require, 'trouble')
+
+      util.map('n', 'gD', vim.lsp.buf.declaration, bufopts)
+      util.map('n', 'gd', vim.lsp.buf.definition, bufopts)
+      -- util.map('n', 'K', vim.lsp.buf.hover, bufopts) -- Defined in keymap.lua
+      if not has_trouble then
+        util.map('n', 'gr', vim.lsp.buf.references, bufopts)
+        util.map('n', 'gi', vim.lsp.buf.implementation, bufopts)
+      end
+      util.map('i', '<c-k>', vim.lsp.buf.signature_help, bufopts)
+      util.map('n', '<leader>T', vim.lsp.buf.type_definition, bufopts)
+      util.map('n', '<leader>R', vim.lsp.buf.rename, bufopts)
+      util.map('n', '<leader>ac', vim.lsp.buf.code_action, bufopts)
+
+      local filetype = vim.bo[bufnr].filetype
+      if formatters[filetype] ~= nil then
+        util.map(
+          'n', '<leader>f',
+          function()
+            vim.lsp.buf.format({ name = formatters[filetype] })
+            vim.api.nvim_feedkeys('zx', 'n', false)
+          end,
+          bufopts
+        )
+      else
+        util.map('n', '<leader>f', function() print('no formatter configured for ' .. filetype) end, bufopts)
+      end
+    end
 
     require('neodev').setup({})
 
