68b2f92c1073851a2e240541d7fa3b6565430b9c
w1ck3dg0ph3r <w1ck3dg0ph3r@gmail.com>
2024-08-01 16:40:59 +0400

    neovim: change spectre mappings

--- nvim/lua/plugins/ui/spectre.lua
+++ nvim/lua/plugins/ui/spectre.lua
@@ -5,5 +5,18 @@ return {
     'nvim-lua/plenary.nvim'
   },
 
-  config = true,
+  config = function()
+    local spectre = require('spectre')
+    local util = require('util')
+    spectre.setup({
+      mapping = {
+        ['send_to_qf'] = {
+          map = "<leader>f",
+          cmd = "<cmd>lua require('spectre.actions').send_to_qf(); require('spectre').toggle()<cr>",
+          desc = "send all items to quickfix"
+        },
+      },
+    })
+    util.map('n', '<leader>S', function() spectre.toggle() end)
+  end,
 }
