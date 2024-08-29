ba62f6ff9c5e8aba340270bb48da4196ba844ce5
w1ck3dg0ph3r <w1ck3dg0ph3r@gmail.com>
2024-08-29 09:30:41 +0400

    neovim: switch to fixed harpoon

--- nvim/lua/plugins/ui/harpoon.lua
+++ nvim/lua/plugins/ui/harpoon.lua
@@ -1,5 +1,7 @@
-return {
-  'ThePrimeagen/harpoon',
+local v1config = {
+  -- 'ThePrimeagen/harpoon',
+  'w1ck3dg0ph3r/nvim-harpoon',
+  branch = 'fix-nav-file-column',
 
   dependencies = {
     'nvim-lua/plenary.nvim',
@@ -19,3 +21,29 @@ return {
     end
   end
 }
+
+local v2config =  {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+  },
+
+  config = function()
+    local harpoon = require('harpoon')
+    harpoon:setup()
+    local util = require('util')
+
+    util.map('n', 'mm', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+    util.map('n', 'ma', function() harpoon:list():add() end)
+    util.map('n', 'mn', function() harpoon:list():next() end)
+    util.map('n', 'mp', function() harpoon:list():prev() end)
+
+    for i = 1, 9, 1 do
+      util.map('n', 'm' .. i, function() harpoon:list():select(i) end)
+    end
+  end
+}
+
+return v1config
