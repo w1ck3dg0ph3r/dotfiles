377e99ea93c5c1681731a022e911e15466964ed4
w1ck3dg0ph3r <w1ck3dg0ph3r@gmail.com>
2024-09-19 10:29:26 +0400

    neovim: fix extending plugin specs from .nvimrc.lua

--- nvim/lua/config/nvimrc.lua
+++ nvim/lua/config/nvimrc.lua
@@ -12,6 +12,7 @@ function M.config()
     local ok, config = pcall(dofile, nvimrc_path)
     if ok then
       nvimrc_config = config or {}
+      print("loaded " .. nvimrc_path)
     else
       print('error loading "' .. nvimrc_path .. '": ' .. config)
       nvimrc_config = {}
@@ -22,14 +23,16 @@ function M.config()
 end
 
 function M.extend_plugin_specs(plugin_specs)
-  if nvimrc_config == nil or nvimrc_config.plugins == nil then
+  local cfg = M.config()
+  if cfg == nil or cfg.plugins == nil then
     return
   end
 
-  for _, spec in pairs(nvimrc_config.plugins) do
+  for _, spec in pairs(cfg.plugins) do
     if spec[1] ~= nil and type(spec[1]) == 'string' then
       local name = spec[1]
       if plugin_specs[name] ~= nil then
+        -- print('extending plugin ' .. name .. ' with:\n' .. vim.inspect(spec))
         plugin_specs[name] = vim.tbl_deep_extend('force', plugin_specs[name], spec)
       end
     end
