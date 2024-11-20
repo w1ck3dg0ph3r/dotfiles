bf679510f82455d8b8f6f5edc67e2cec0ce03a3a
w1ck3dg0ph3r <w1ck3dg0ph3r@gmail.com>
2024-11-20 12:20:46 +0400

    neovim: reorganize options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,60 +1,61 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-vim.o.termguicolors = true
-vim.o.lazyredraw = true
-
-vim.o.timeout = true
-vim.o.timeoutlen = 500
-
-vim.o.backup = false
-vim.o.writebackup = false
-vim.o.swapfile = false
-
--- Configure leader
-vim.keymap.set('n', '<space>', '<nop>')
-vim.g.mapleader = ' '
-
-vim.o.mouse = 'a'
-
-vim.o.clipboard = 'unnamedplus'
-
-vim.o.completeopt = 'menu,menuone,noselect'
-
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.lazyredraw = true
 
-vim.o.wrap = false
-vim.o.formatoptions = 'cronjq'
+vim.keymap.set('n', '<space>', '<nop>')
+vim.g.mapleader = ' '
+vim.opt.timeout = true
+vim.opt.timeoutlen = 500
 
-vim.o.tabstop = 2
-vim.o.softtabstop = 2
-vim.o.shiftwidth = 2
-vim.o.expandtab = true
-vim.o.smartindent = true
-vim.o.breakindent = true
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
 
-vim.o.hlsearch = true
-vim.o.incsearch = true
-vim.o.ignorecase = true
-vim.o.smartcase = true
+vim.opt.mouse = 'a'
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.clipboard = 'unnamedplus'
+
+vim.opt.completeopt = { 'menuone', 'noselect' }
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+
+vim.opt.wrap = false
+vim.opt.formatoptions = 'cronjq'
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.breakindent = true
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 -- Visible whitespace
-vim.o.list = true
-vim.opt.listchars:append 'space:⋅'
-vim.opt.listchars:append 'tab:» '
+vim.opt.list = true
+vim.opt.listchars:append('space:⋅')
+vim.opt.listchars:append('tab:» ')
 
 -- Code folding
-vim.o.foldlevel = 99
-vim.o.foldmethod = 'expr'
-vim.o.foldnestmax = 10
-vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
-vim.o.foldcolumn = '1'
+vim.opt.foldlevel = 99
+vim.opt.foldmethod = 'expr'
+vim.opt.foldnestmax = 10
+vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+vim.opt.foldcolumn = '1'
 
 vim.g.omni_sql_no_default_maps = true -- Disable default sql completion with c-c
 
