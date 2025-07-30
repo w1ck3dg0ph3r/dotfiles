2ad90fd94eec525d230e6dce0c674cf588e96e04
w1ck3dg0ph3r <w1ck3dg0ph3r@gmail.com>
2025-07-30 12:21:21 +0400

    neovim: move spell file out of config dir

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -58,6 +58,9 @@ vim.opt.foldnestmax = 10
 vim.opt.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
 vim.opt.foldcolumn = '1'
 
+-- Spelling
+vim.opt.spellfile = vim.fn.stdpath('data') .. '/spell/en.utf-8.add'
+
 -- Diagnostic icons
 vim.diagnostic.config({
   virtual_text = {
