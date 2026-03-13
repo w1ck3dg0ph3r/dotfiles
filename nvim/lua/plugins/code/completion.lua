acc960689819d356fdcd69c54658122fa08f546d
w1ck3dg0ph3r <w1ck3dg0ph3r@gmail.com>
2026-03-13 16:48:22 +0400

    nvim: tweak completion config

--- nvim/lua/plugins/code/completion.lua
+++ nvim/lua/plugins/code/completion.lua
@@ -100,8 +100,17 @@ return {
         expandable_indicator = true,
       },
 
+      matching = {
+        disallow_fuzzy_matching = false,
+        disallow_fullfuzzy_matching = true,
+        disallow_partial_fuzzy_matching = false,
+        disallow_partial_matching = false,
+        disallow_prefix_unmatching = false,
+        disallow_symbol_nonprefix_matching = false,
+      },
+
       sources = cmp.config.sources({
-        { name = 'nvim_lsp', priority = 15 },
+        { name = 'nvim_lsp', priority = 20 },
         { name = 'lazydev',  priority = 11 },
         { name = 'luasnip',  priority = 10 },
         { name = 'path',     priority = 5 },
@@ -112,13 +121,18 @@ return {
       sorting = {
         priority_weight = 2.5,
         comparators = {
-          cmp.config.compare.offset,    -- Entries with smaller offset will be ranked higher.
-          cmp.config.compare.exact,     -- Entries with exact == true will be ranked higher.
-          cmp.config.compare.score,     -- Entries with higher score will be ranked higher.
-          -- cmp.config.compare.length,    -- Entires with shorter label length will be ranked higher.
-          cmp.config.compare.sort_text, -- Entries will be ranked according to the lexicographical order of sortText.
-          cmp.config.compare.order,     -- Entries with smaller id will be ranked higher.
-          cmp.config.compare.kind,      -- Entires with smaller ordinal value of 'kind' will be ranked higher.
+          cmp.config.compare.score, -- Entries with higher score will be ranked higher.
+          cmp.config.compare.order, -- Entries with smaller id will be ranked higher.
+
+          -- Available comparators:
+
+          -- cmp.config.compare.offset,        -- Entries with smaller offset will be ranked higher.
+          -- cmp.config.compare.exact,         -- Entries with exact == true will be ranked higher.
+          -- cmp.config.compare.score,         -- Entries with higher score will be ranked higher.
+          -- cmp.config.compare.length,        -- Entires with shorter label length will be ranked higher.
+          -- cmp.config.compare.sort_text,     -- Entries will be ranked according to the lexicographical order of sortText.
+          -- cmp.config.compare.kind,          -- Entires with smaller ordinal value of 'kind' will be ranked higher.
+          -- cmp.config.compare.order,         -- Entries with smaller id will be ranked higher.
           -- cmp.config.compare.locality,      -- Entries with higher locality (i.e., words that are closer to the cursor) will be ranked higher. See GH-183 for more details.
           -- cmp.config.compare.recently_used, -- Entries that are used recently will be ranked higher.
           -- cmp.config.compare.scopes,        -- Entries defined in a closer scope will be ranked higher (e.g., prefer local variables to globals).
